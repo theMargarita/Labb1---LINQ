c85dbc9213e6f6e0b891c40a7e6f8d34c8c0391c
theMargarita <margotvoistinova@hotmail.se>
2025-04-14 18:26:55 +0200

    yeah I might not pass this, oh well

--- Labb1 - LINQ/Program.cs
+++ Labb1 - LINQ/Program.cs
@@ -4,8 +4,7 @@
     {
         static void Main(string[] args)
         {
-            //Data.GetCategoryAndProducts();
-            //Data.GetTopThreeMostSoldOrderDetailData();
+
 
             Console.WriteLine("" +
                 "1: Hämta alla produkter i kategorin \"Electronics\" och sortera dem efter pris (högst först)\n" +
@@ -13,7 +12,7 @@
                 "3: Get total amount of the past month amount \n" +
                 "4: Most Sold Item\n" +
                 "5: Get  Categorys and their products\n" +
-                "6: \n" +
+                "6: Hämta alla ordrar med tillhörande kunduppgifter och orderdetaljer där totalbeloppet överstiger 1000 kr\n" +
                 "7:Shut of the program " +
                 "");
 
@@ -23,18 +22,27 @@
                 switch (input)
                 {
                     case "1":
+                        Console.Clear();
+
                         Data.GetElectronices();
+                        //Console.ReadKey();
                         break;
 
                     case "2":
+                        Console.Clear();
+
                         Data.GetSuppliers();
                         break;
 
                     case "3":
+                        Console.Clear();
+
                         Data.GetOrders();
                         break;
 
                     case "4":
+                        Console.Clear();
+
                         Data.GetTopThreeMostSoldOrderDetailData();
                         break;
 
